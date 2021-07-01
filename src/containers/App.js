9d1e75272518736d13b62f8b29552c07ab548928
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-07-01 14:03:19 +0530

    React Hooks Introduced

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,67 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super();
+function App() {
+    // No longer a class
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ""
+    //     }
+    // }
 
-        this.state = {
-            robots: [],
-            searchfield: ""
-        }
-    }
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    const [count, setCount] = useState(0);
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => { return response.json() })
+    //         .then(users => { this.setState({ robots: users }); })
+    // }
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => { return response.json() })
-            .then(users => { this.setState({ robots: users }); })
+            .then(users => { setRobots(users) })
+        console.log(count);
+    }, [count])
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
+    // render() {
+    // const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    //console.log(filteredRobots);
+    /* Longer Method
+    if (robots.length === 0) {
+        return <h1 className="tc f1">Loading...</h1>
     }
-
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        });
-        //console.log(filteredRobots);
-        /* Longer Method
-        if (robots.length === 0) {
-            return <h1 className="tc f1">Loading...</h1>
-        }
-        else {
-            return (
-                <div className="tc" >
-                    <h1 className="f1">Robo Friends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
+    else {
+        return (
+            <div className="tc" >
+                <h1 className="f1">Robo Friends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+        );
+    }
+    */
+    return !robots.length ?
+        <h1 className="tc f1">Loading...</h1> :
+        (
+            <div className="tc" >
+                <h1 className="f1">Robo Friends</h1>
+                <button onClick={() => setCount(count + 1)}>Click Me!</button>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundary>
                         <CardList robots={filteredRobots} />
-                    </Scroll>
-                </div>
-            );
-        }
-        */
-        return !robots.length ?
-            <h1 className="tc f1">Loading...</h1> :
-            (
-                <div className="tc" >
-                    <h1 className="f1">Robo Friends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            )
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
+        )
 
-    }
+    // }
 }
 
 export default App;
